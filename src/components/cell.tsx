275ed22151d7789bee8208900237ac53fd98f91e
odelya krief <odelya.kr@gmail.com>
2023-10-25 20:26:59 +0300

    cell: fix square hight changes.

--- src/components/cell.tsx
+++ src/components/cell.tsx
@@ -2,7 +2,7 @@ const CellComponent = ({ value, index, onClick }) => {
   return (
     <button
       className="text-gray-800 content-center bg-white text-5xl
-       hover:bg-gray-200 disabled:bg-white"
+       hover:bg-gray-200 disabled:bg-white aspect-square"
       onClick={() => onClick(index)}
       disabled={value != ""}
     >
